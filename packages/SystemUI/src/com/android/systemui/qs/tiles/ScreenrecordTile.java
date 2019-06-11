mike2587/abc_frameworks_base

Create an advanced screenrecord system [1/2]
part 2/2 is in frameworks native

Multi ScreenRecord tile, blinking stop button, screenrecord service improvements [1/2]

single tap to toggle quality:
- 480*800 resolution and 1.5Mbps
- default resolution (720p) and 4Mbps
- default resolution (720p) and 8Mbps
long press to start recording

Show also a blinking red button at bottom right of the screen
that can be tapped to stop the recording instead of using
the notification.

Also improve the screenrecord service and be sure
to stop it when we are done.

part 2/2 is in framework/av tree, it just sets max recording
duration to 30 minutes instead of 3

Change-Id: I9a0161e6ddafb721421585c7f16bba4b30319425

Screenrec: let hide the red stop button but keep it clickable

thanks to @KreAch3R for this great idea.

Here we add an option to the screenrecord notification to hide
the red stop hint at bottom right so it doesn't appear in our
recordings, but it's still clickable to stop the service.

Change-Id: I1b93b10f07c415c0149097635d78634e0d227932

Screenrecord: show a Share/Delete notification when stopped

also fix MediaScannerConnection memory leak due to aosp bug:
https://issuetracker.google.com/issues/37046656
manually connecting and disconnecting the client

Change-Id: Ia8ea68dd4c4957d14294d0c1bf24f15daac122cb

Screenrecord red hint: long press on it to move to the opposite side

we can already hide it from notification but we still keep it clickable,
but in some cases we would need to click for example on the keyboard
search/enter button covered by the hint.

Change-Id: Ibd541987a0c3ea4be6ea7016b38f248d83431de1

Better strings for screenrec tile

Change-Id: I88822f736d07e05d2464275e2d07b19a6a86fac7

Screenrecord notification: tap on it to open the video

Change-Id: Ib92c9d37ad9ea107b01bca630ae9231b69ebbf18

Screenrecord: show chrono and total recording duration/size

Change-Id: Ic5c72348cf5848d6252f32ef49cec54a375e9f2f

Simplify successful screenrecord notification strings

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
@@ -1,0 +1,110 @@
+/*
+ * Copyright (C) 2017 ABC rom
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.WindowManager;
+
+import com.android.internal.util.abc.AbcUtils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+/** Quick settings tile: Screenrecord **/
+public class ScreenrecordTile extends QSTileImpl<BooleanState> {
+
+    private static final int SCREEN_RECORD_LOW_QUALITY = WindowManager.SCREEN_RECORD_LOW_QUALITY;
+    private static final int SCREEN_RECORD_MID_QUALITY = WindowManager.SCREEN_RECORD_MID_QUALITY;
+    private static final int SCREEN_RECORD_HIGH_QUALITY = WindowManager.SCREEN_RECORD_HIGH_QUALITY;
+
+    private int mMode = SCREEN_RECORD_LOW_QUALITY;
+
+    public ScreenrecordTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ABC;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {}
+
+    @Override
+    public void handleClick() {
+        switchMode();
+        refreshState();
+    }
+
+
+    private void switchMode() {
+        if (mMode == SCREEN_RECORD_LOW_QUALITY) {
+            mMode = SCREEN_RECORD_MID_QUALITY;
+        } else if (mMode == SCREEN_RECORD_MID_QUALITY) {
+            mMode = SCREEN_RECORD_HIGH_QUALITY;
+        } else if (mMode == SCREEN_RECORD_HIGH_QUALITY) {
+            mMode = SCREEN_RECORD_LOW_QUALITY;
+        }
+    }
+
+    @Override
+    public void handleLongClick() {
+        mHost.collapsePanels();
+        //finish collapsing the panel
+        try {
+             Thread.sleep(1000); //1s
+        } catch (InterruptedException ie) {}
+        AbcUtils.takeScreenrecord(mMode);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_screenrecord_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_screenrecord_label);
+        if (mMode == SCREEN_RECORD_LOW_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_lq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_lq);
+        } else if (mMode == SCREEN_RECORD_MID_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_mq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_mq);
+        } else if (mMode == SCREEN_RECORD_HIGH_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_hq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_hq);
+        }
+    }
+}

LotusOS-Android/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
@@ -1,0 +1,111 @@
+/*
+ * Copyright (C) 2017 ABC rom
+ * Copyright (C) 2019 The Evolution X Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.WindowManager;
+
+import com.android.internal.util.evolution.EvolutionUtils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+/** Quick settings tile: Screenrecord **/
+public class ScreenrecordTile extends QSTileImpl<BooleanState> {
+
+    private static final int SCREEN_RECORD_LOW_QUALITY = WindowManager.SCREEN_RECORD_LOW_QUALITY;
+    private static final int SCREEN_RECORD_MID_QUALITY = WindowManager.SCREEN_RECORD_MID_QUALITY;
+    private static final int SCREEN_RECORD_HIGH_QUALITY = WindowManager.SCREEN_RECORD_HIGH_QUALITY;
+
+    private int mMode = SCREEN_RECORD_LOW_QUALITY;
+
+    public ScreenrecordTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVO;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {}
+
+    @Override
+    public void handleClick() {
+        switchMode();
+        refreshState();
+    }
+
+
+    private void switchMode() {
+        if (mMode == SCREEN_RECORD_LOW_QUALITY) {
+            mMode = SCREEN_RECORD_MID_QUALITY;
+        } else if (mMode == SCREEN_RECORD_MID_QUALITY) {
+            mMode = SCREEN_RECORD_HIGH_QUALITY;
+        } else if (mMode == SCREEN_RECORD_HIGH_QUALITY) {
+            mMode = SCREEN_RECORD_LOW_QUALITY;
+        }
+    }
+
+    @Override
+    public void handleLongClick() {
+        mHost.collapsePanels();
+        //finish collapsing the panel
+        try {
+             Thread.sleep(1000); //1s
+        } catch (InterruptedException ie) {}
+        EvolutionUtils.takeScreenrecord(mMode);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_screenrecord_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_screenrecord_label);
+        if (mMode == SCREEN_RECORD_LOW_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_lq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_lq);
+        } else if (mMode == SCREEN_RECORD_MID_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_mq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_mq);
+        } else if (mMode == SCREEN_RECORD_HIGH_QUALITY) {
+            state.label = mContext.getString(R.string.quick_settings_screenrecord_hq_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenrecord_hq);
+        }
+    }
+}