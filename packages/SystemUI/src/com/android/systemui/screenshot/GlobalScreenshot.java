83ba85f69d479abc037f183a3e6dbb87fd9b230d
Hưng Phan <phandinhhungvp2001@gmail.com>
2019-06-11 20:29:55 +0700

    Fix screenshot not saved when appending appname with some languages
    like Virgin Islands English when taking a screenshot of the Settings app
    
    todo: find a less noob way, if possible, to handle this
    
    Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -80,7 +80,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
-import com.android.internal.util.evolution.EvolutionUtils;
+import com.android.internal.util.lotus.LotusUtils;
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
 import com.android.systemui.util.NotificationChannels;
@@ -148,7 +148,7 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         String imageDate = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date(mImageTime));
         mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
         final PackageManager pm = context.getPackageManager();
-        ActivityInfo info = EvolutionUtils.getRunningActivityInfo(context);
+        ActivityInfo info = LotusUtils.getRunningActivityInfo(context);
         if (info != null) {
             CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
             boolean onKeyguard = context.getSystemService(KeyguardManager.class).isKeyguardLocked();
@@ -165,7 +165,7 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
                 // now remove all special chars from the app name
                 appNameString = appNameString.replaceAll("[\\\\/:*?\"<>|\\s]+", "_");
 
-                mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME, imageDate, appNameString);
+                mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME, appNameString, imageDate);
             }
         }
 
@@ -669,7 +669,7 @@ class GlobalScreenshot {
                         view.setVisibility(View.GONE);
                         mWindowManager.removeView(mScreenshotLayout);
                         final Rect rect = view.getSelectionRect();
-                        if (rect != null && rect.width() != 0 && rect.height() != 0) {
+                        if (rect != null && !rect.isEmpty()) {
                             // Need mScreenshotLayout to handle it after the view disappears
                             mScreenshotLayout.post(new Runnable() {
                                 public void run() {
@@ -1003,7 +1003,6 @@ class GlobalScreenshot {
                     (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             final Uri uri = Uri.parse(intent.getStringExtra(SCREENSHOT_URI_ID));
             nm.cancel(SystemMessage.NOTE_GLOBAL_SCREENSHOT);
-            Toast.makeText(context, R.string.delete_screenshot_toast, Toast.LENGTH_SHORT).show();
 
             // And delete the image from the media store
             new DeleteImageInBackgroundTask(context).execute(uri);
