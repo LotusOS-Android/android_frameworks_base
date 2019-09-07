739a5a42adb30b46d70abef0a3a90a5eeb46cbf9
hungphan2001 <phandinhhungvp2001@gmail.com>
2019-09-07 18:23:00 +0700

    Revert "base: Q seekbar: Add extra tweaks"
    This reverts commit a5cd47084355e7471f6023edbdde40068e0af0fc.

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationMediaTemplateViewWrapper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationMediaTemplateViewWrapper.java
@@ -162,11 +162,9 @@ public class NotificationMediaTemplateViewWrapper extends NotificationTemplateVi
                 if (mSeekBarTimer == null) {
                     if (canSeekMedia()) {
                         mSeekBar.getThumb().setAlpha(255);
-                        mSeekBar.setAlpha(255);
                         mSeekBar.setEnabled(true);
                     } else {
                         mSeekBar.getThumb().setAlpha(0);
-                        mSeekBar.setAlpha(190);
                         mSeekBar.setEnabled(false);
                     }
                     startTimer();
